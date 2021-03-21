8e705951f31e8b409e3bb170b9fe4bd03166264e
Andrew <atg27@uw.edu>
2021-03-21 13:16:29 -0700

    commit tree 1203be02d8943dd4efd063fb8eb607c08a132b8e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching (inputArray, inputName) {
+    return inputArray.filter(matches => matches.toUpperCase() === inputName.toUpperCase());
+}
+
+
+// function filterItems(arr, query) {
+//     return arr.filter(function(el) {
+//         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+
+function fuzzyMatch(inputArray, inputName) {
+    return inputArray.filter(matches => matches.toLowerCase().indexOf(inputName.toLowerCase()) === 0);
+}
+
+function matchName(inputArray, inputName) {
+    return inputArray.filter(matches => matches.name === inputName);
+}
